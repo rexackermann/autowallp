b291905cf72975fd5fc87bbe00dd5000996549fb
RexAckermann <ackermann88888@gmail.com>
2022-06-03 16:16:54 +0600

    first commit

--- install.sh
+++ install.sh
@@ -4,4 +4,5 @@
 sudo cp -rfv target/release/autowallp /usr/local/bin/
 cp -rfv autowallp ~/.config/autowallp
 sudo chmod +x /usr/local/bin/autowallp
+mkdir ~/.config/autostart/
 cp -rfv autowallp.desktop ~/.config/autostart/

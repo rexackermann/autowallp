055fe09e55aec06e26399200cc6e6d1c5774edb7
Rex Ackermann <ackermann88888@gmail.com>
2023-06-29 20:02:37 +0600

    update_1688047357

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/env bash
-
+#!/bin/env bash
 git clone https://github.com/rexackermann/autowallp.git
 cd autowallp
 sudo cp -rfv ./autowallp/autowallp /usr/local/bin/
-cp -rfv ./autowallp ~/.config/autowallp
+sudo cp -rfv ./autowallp/wallch /usr/local/bin/
+cp -rfv ./autowallp "${XDG_CONFIG_HOME:-"$HOME"/.config}/"
 sudo chmod +x /usr/local/bin/autowallp
 mkdir ~/.config/autostart/
 cp -rfv autowallp.desktop ~/.config/autostart/
